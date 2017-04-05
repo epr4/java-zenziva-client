49146a6e069cf6137da56d1b811dc7da27d9d0e1
Eko Prasetiyo <ekop96@gmail.com>
2017-04-05 16:58:33 +0700

    init

--- src/main/java/e/Mask.java
+++ src/main/java/e/Mask.java
@@ -1,7 +1,7 @@
 package e;
 
-import org.springframework.web.client.RestTemplate;
 import org.springframework.http.converter.xml.SimpleXmlHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 
 /**
@@ -12,8 +12,9 @@ public class Mask {
     public static void main(String[] args) {
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.getMessageConverters().add(new SimpleXmlHttpMessageConverter());
-        final String url = "http://10.0.2.2:8080/hello-world";
-        Responze responze = restTemplate.getForObject(url, Responze.class);
+        final String url = "http://alpha.zenziva.net/apps/smsapi.php?userkey=xxx&passkey=xxx&nohp=xxx&pesan=xxx";
+        Response response = restTemplate.getForObject(url, Response.class);
+        System.out.println("response = " + response);
     }
 
 }
