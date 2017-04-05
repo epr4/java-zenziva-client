4487422e83e993f8f7e6f63e1337ea4e84ad7663
Eko Prasetiyo <ekop96@gmail.com>
2017-04-05 17:00:53 +0700

    Add some classes

---
+++ src/main/java/e/Message.java
@@ -0,0 +1,11 @@
+package e;
+
+/**
+ * Created by eco on 05/04/2017.
+ */
+public class Message {
+
+    int status;
+    String text;
+
+}
---
+++ src/main/java/e/Response.java
@@ -0,0 +1,10 @@
+package e;
+
+/**
+ * Created by eco on 05/04/2017.
+ */
+public class Response {
+
+    Message message;
+
+}
