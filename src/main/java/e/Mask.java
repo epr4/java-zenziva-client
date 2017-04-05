b695e6aaab9caf629986294c85eba339d36cdc0b
Eko Prasetiyo <ekop96@gmail.com>
2017-04-05 16:44:36 +0700

    init

--- src/main/java/e/Mask.java
+++ src/main/java/e/Mask.java
@@ -12,6 +12,7 @@ public class Mask {
     public static void main(String[] args) {
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.getMessageConverters().add(new SimpleXmlHttpMessageConverter());
+        final String url = "http://10.0.2.2:8080/hello-world";
         Responze responze = restTemplate.getForObject(url, Responze.class);
     }
 
